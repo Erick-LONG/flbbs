e87d3260de2852d4860d8042bed043caa3097093
fulong <834424581@qq.com>
2018-06-01 16:04:34 +0800

    利用pillow生成图形验证码

--- apps/front/views.py
+++ apps/front/views.py
@@ -2,14 +2,31 @@ from flask import (
     Blueprint,
     views,
     render_template,
-
+    make_response
 )
 
+from utils.captcha import Captcha
+from io import BytesIO
+
 front_bp = Blueprint('front',__name__)
+
 
 @front_bp.route('/')
 def index():
     return 'front index'
+
+
+@front_bp.route('/captcha/')
+def graph_captcha():
+    #获取验证码
+    text,image = Captcha.gene_graph_captcha()
+    #BytesIO,字节流
+    out = BytesIO()
+    image.save(out,'png')
+    out.seek(0)
+    resp = make_response(out.read())
+    resp.content_type = 'image/png'
+    return resp
 
 
 class SignupView(views.MethodView):
