6df7df327f896442305b19eaf2b9e168f7400cbe
fulong <834424581@qq.com>
2018-06-01 15:18:15 +0800

    前台注册页面搭建

--- apps/front/views.py
+++ apps/front/views.py
@@ -1,7 +1,20 @@
-from flask import Blueprint
+from flask import (
+    Blueprint,
+    views,
+    render_template,
+
+)
 
 front_bp = Blueprint('front',__name__)
 
 @front_bp.route('/')
 def index():
     return 'front index'
+
+
+class SignupView(views.MethodView):
+    def get(self):
+        return render_template('front/front_signup.html')
+
+
+front_bp.add_url_rule('/signup/',view_func=SignupView.as_view('signup'))
