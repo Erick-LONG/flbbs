f2ac48d522799e4bb1ab278bd5a5cc7d56ca1fc8
fulong <834424581@qq.com>
2018-06-04 13:57:17 +0800

    短信接口验证码接口加密，js代码加密与混淆

--- apps/common/views.py
+++ apps/common/views.py
@@ -2,22 +2,40 @@ from flask import Blueprint,request
 from exts import alidayu
 from utils import restful
 from utils.captcha import Captcha
+from .forms import SMSCaptchaForm
 
 common_bp = Blueprint('common',__name__,url_prefix='/c')
 
 
-@common_bp.route('/sms_captcha/')
-def sms_captcha():
-    #?telephone=xxx
-    telephone = request.args.get('telephone')
-    if not telephone:
-        return restful.params_error(message='请传入手机号码')
+# @common_bp.route('/sms_captcha/')
+# def sms_captcha():
+#     #?telephone=xxx
+#     telephone = request.args.get('telephone')
+#     if not telephone:
+#         return restful.params_error(message='请传入手机号码')
+#
+#     captcha = Captcha.gene_text(number=4)
+#     if alidayu.send_sms(telephone,code=captcha):
+#         return restful.success()
+#     else:
+#         return restful.params_error(message='短信验证码发送失败')
 
-    captcha = Captcha.gene_text(number=4)
-    if alidayu.send_sms(telephone,code=captcha):
-        return restful.success()
+@common_bp.route('/sms_captcha/',methods=['POST'])
+def sms_captcha():
+    #telephone
+    #timestamp
+    #md5(ts +telephone+salt)
+    form  = SMSCaptchaForm(request.form)
+    if form.validate():
+        telephone = form.telephone.data
+        captcha = Captcha.gene_text(number=4)
+        if alidayu.send_sms(telephone,code=captcha):
+            return restful.success()
+        else:
+            return restful.params_error(message='短信验证码发送失败')
     else:
-        return restful.params_error(message='短信验证码发送失败')
+        return restful.params_error(message='参数错误')
+
 
 
 
