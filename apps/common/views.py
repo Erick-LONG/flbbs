47a8b198527991dc5fbe8072694af17390deb59c
fulong <834424581@qq.com>
2018-06-04 13:01:02 +0800

    短信接口验证码接口

--- apps/common/views.py
+++ apps/common/views.py
@@ -1,7 +1,23 @@
-from flask import Blueprint
+from flask import Blueprint,request
+from exts import alidayu
+from utils import restful
+from utils.captcha import Captcha
 
-common_bp = Blueprint('common',__name__,url_prefix='/common')
+common_bp = Blueprint('common',__name__,url_prefix='/c')
 
-@common_bp.route('/')
-def index():
-    return 'common index'
+
+@common_bp.route('/sms_captcha/')
+def sms_captcha():
+    #?telephone=xxx
+    telephone = request.args.get('telephone')
+    if not telephone:
+        return restful.params_error(message='请传入手机号码')
+
+    captcha = Captcha.gene_text(number=4)
+    if alidayu.send_sms(telephone,code=captcha):
+        return restful.success()
+    else:
+        return restful.params_error(message='短信验证码发送失败')
+
+
+
